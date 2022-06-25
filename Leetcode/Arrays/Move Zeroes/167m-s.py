297367ed7ca256c87118d28d4821424c2b2d1065
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-06-25 17:58:30 +0530

    Create 167m-s.py

---
+++ Leetcode/Arrays/Move Zeroes/167m-s.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
