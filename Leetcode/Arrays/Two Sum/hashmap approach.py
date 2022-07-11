4672a3a3203f3631263d97fd0c53467b3ac3c4af
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-07-11 16:35:46 +0530

    Create hashmap approach.py

---
+++ Leetcode/Arrays/Two Sum/hashmap approach.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for i in range(len(nums)):
+            if target-nums[i] in hashmap:
+                return [hashmap[(target-nums[i])],i]
+            hashmap[nums[i]]=i
