c5126b6e32718088373c812fc70b5afff8f689c3
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-07-20 13:26:01 +0530

    Update hashmap approach.py

--- Leetcode/Arrays/Two Sum/hashmap approach.py
+++ Leetcode/Arrays/Two Sum/hashmap approach.py
@@ -4,4 +4,8 @@ class Solution:
         for i in range(len(nums)):
             if target-nums[i] in hashmap:
                 return [hashmap[(target-nums[i])],i]
+        #here we add element in hash map only after iterating to it because we do not get a duplicated value where we add up the same element twice
+        # consider [2,1,3,5,7] and target=4
+        # here we do this stept to get 1,3 and not 2,2
             hashmap[nums[i]]=i
+            
