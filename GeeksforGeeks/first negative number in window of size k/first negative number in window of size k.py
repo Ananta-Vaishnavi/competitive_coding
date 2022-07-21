ac02c32ae99b8001d67d6f3d18a75730e598c425
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-07-21 12:33:41 +0530

    Create first negative number in window of size k.py

---
+++ GeeksforGeeks/first negative number in window of size k/first negative number in window of size k.py
@@ -0,0 +1,21 @@
+def FirstNegativeInteger( A, N, K):
+    answer=[]
+    neg=[]
+    for i in range(K):
+        if A[i]<0:
+            neg.append(A[i])
+    start=0 
+    end=K-1
+    while end!=len(A):
+        if A[end]<0 and start!=0:
+            neg.append(A[end])
+        if start>0 and A[start-1]==neg[0]:
+            neg.pop(0)
+        if neg:
+            answer.append(neg[0])
+        else:
+            answer.append(0)
+        end+=1
+        start+=1
+    return answer
+print(FirstNegativeInteger( A, N, K))
