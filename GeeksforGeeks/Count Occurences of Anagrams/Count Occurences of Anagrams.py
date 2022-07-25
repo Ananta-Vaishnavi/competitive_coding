80eacc22163c0f575fcad1ce467cb0a8ede6ca9f
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-07-25 14:50:48 +0530

    Update Count Occurences of Anagrams.py

--- GeeksforGeeks/Count Occurences of Anagrams/Count Occurences of Anagrams.py
+++ GeeksforGeeks/Count Occurences of Anagrams/Count Occurences of Anagrams.py
@@ -1,29 +1,34 @@
+#User function Template for python3
 from collections import Counter
-txt = 'aabaabaa'
-pat = 'aaba'
-def search(pat, txt):
-    answer=0
-    original_count=dict(Counter(pat))
-    count_track={}
-    for i in range(len(pat)):
-        if txt[i] in count_track:
-            count_track[txt[i]]+=1 
-        else:
-            count_track[txt[i]]=1
-    start=0
-    end=len(pat)-1
-    while end!=len(txt):
-        if txt[end] in count_track:
-            count_track[txt[end]]+=1 
-        else:
-            count_track[txt[end]]=1
-        if start>0 and count_track[txt[start-1]]==1:
-            count_track.pop(txt[start-1])
-        else:
-            count_track[txt[start-1]]-=1
+class Solution:
+
+	
+	def search(self,pat, txt):
+        answer=0
+        original_count=dict(Counter(pat))
+        count_track={}
+        for i in range(len(pat)):
+            if txt[i] in count_track:
+                count_track[txt[i]]+=1 
+            else:
+                count_track[txt[i]]=1
         if original_count==count_track:
             answer+=1
-        end+=1 
-        start+=1 
-    return answer
-print(search(pat,txt))
+        start=1
+        end=len(pat)
+        while end<len(txt):
+            
+            if txt[end] in count_track:
+             count_track[txt[end]]+=1 
+            else:
+                count_track[txt[end]]=1
+            
+            if count_track[txt[start-1]]==1:
+                count_track.pop(txt[start-1])
+            else:
+                count_track[txt[start-1]]-=1
+            if original_count==count_track:
+                answer+=1
+            end+=1 
+            start+=1 
+        return answer
