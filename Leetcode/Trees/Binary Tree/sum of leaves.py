5a6c018e2e1cdaf95c40959c953d69a239f906f6
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-11-13 19:34:30 +0530

    Create sum of leaves.py

---
+++ Leetcode/Trees/Binary Tree/sum of leaves.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        if not root.left and not root.right:
+            return 1
+        
+        return self.sumOfLeftLeaves(root.left)+self.sumOfLeftLeaves(root.right)
